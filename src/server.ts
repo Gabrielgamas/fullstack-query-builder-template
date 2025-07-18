4a6fac8b5b373283622b1595b26e2114a6a51711
Gabrielgamas <gamas.enge@hotmail.com>
2025-07-18 10:39:14 -0300

    Metodos HTTP

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,32 @@ app.post("/courses", async (request: Request, response: Response) => {
     name,
   });
 
-  response.status(201).json();
+  // await knex.raw("INSERT INTO courses (name) VALUES (?)", [name]);
+
+  return response.status(201).json();
+});
+
+app.get("/courses", async (request: Request, response: Response) => {
+  const courses = await knex("courses").select().orderBy("name", "desc");
+
+  //const courses = await knex.raw("SELECT * FROM courses");
+
+  return response.json(courses);
+});
+
+app.put("/courses/:id", async (request: Request, response: Response) => {
+  const { id } = request.params;
+  const { name } = request.body;
+
+  await knex("courses").update({ name }).where({ id });
+  return response.json();
+});
+
+app.delete("/courses/:id", async (request: Request, response: Response) => {
+  const { id } = request.params;
+
+  await knex("courses").delete().where({ id });
+  return response.json();
 });
 
 app.listen(3333, () => console.log(`Server is running on port 3333`));
